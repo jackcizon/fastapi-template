843d3cd35c2dcec07536296303d2fb5c808e6539
jackcizon <jack20021213cn@gmail.com>
2026-02-01 11:57:19 +0800

    config envs, conf settings

--- src/core/config.py
+++ src/core/config.py
@@ -1,1 +1,28 @@
-"""config settings"""
+import os
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+__all__ = [
+    'settings'
+]
+
+ROOT_DIR = Path(__file__).resolve().parent.parent.parent
+SRC_DIR = os.path.join(ROOT_DIR, "src")
+ENVS_DIR = os.path.join(ROOT_DIR, "envs")
+
+ENV = os.getenv("ENV", "dev")  # 注册到shell的变量列表，默认为dev
+
+
+class Settings(BaseSettings):
+    env: str = "dev"
+    debug: bool = True
+    database_url: str | None = None
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(ENVS_DIR, f".env.{ENV}"),
+        env_file_encoding="utf-8"
+    )
+
+
+# use module level singleton
+settings = Settings()
--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@
 from fastapi import FastAPI
 
 from apps.users.routes import users_router
+from core.config import settings
 
-app = FastAPI(debug=True)
+app = FastAPI(debug=settings.debug)
 
 app.include_router(router=users_router, prefix='/users', tags=['users'])
