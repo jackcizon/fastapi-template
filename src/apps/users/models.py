7931c3afc5ffcd372a0bdda02e6ebc159cf8fc8b
jackcizon <jack20021213cn@gmail.com>
2026-02-01 20:03:46 +0800

    feat: add app:users model

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -1,2 +1,9 @@
-class User:
-    pass
+from sqlmodel.main import SQLModel, Field
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users_User"
+    id: int = Field(None, primary_key=True)
+    name: str
+    age: int = Field(1, ge=1, le=120, description="age[1, 120]")
+    description: str | None = None
