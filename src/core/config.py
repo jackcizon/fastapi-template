064e6a953cd6fe16286f1b88d9f17fdb30ef341d
jackcizon <jack20021213cn@gmail.com>
2026-03-11 18:10:53 +0800

    refactor: re-write settings, del pydantic-settings(it's useless)

--- src/core/config.py
+++ src/core/config.py
@@ -1,56 +1,52 @@
 import os
+import json
 from pathlib import Path
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-__all__ = ["app_settings", "infra_settings"]
 
 ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 SRC_DIR = os.path.join(ROOT_DIR, "src")
 ENVS_DIR = os.path.join(ROOT_DIR, "envs")
 
-ENV = os.getenv("ENV", "dev")  # 注册到shell的变量列表，默认为dev
+ENV_FLAG = os.getenv("ENV", "dev")  # register into os envs, default is dev
+CONFIG_PATH = os.path.join(ENVS_DIR, f'config.{ENV_FLAG}.json')  # read config dynamically
 
 
-class AppSettings(BaseSettings):
-    """app base settings"""
+class Settings:
+    """app settings"""
 
-    env: str = "dev"
-    debug: bool = True
+    @staticmethod
+    def _load() -> dict:
+        with open(CONFIG_PATH, mode='rt', encoding='utf-8') as fp:
+            return json.load(fp=fp)
 
-    model_config = SettingsConfigDict(
-        env_file=os.path.join(ENVS_DIR, f".env.{ENV}.app"),
-        env_file_encoding="utf-8",
-        extra="ignore",
-    )
+    def __init__(self):
+        self.config_dict = self._load()
+        # basic
+        self.env: str = self.config_dict['env']
+        self.debug: bool = self.config_dict['debug']
 
+        # DB
+        self.postgres_user: str = self.config_dict['db']['user']
+        self.postgres_password: str = self.config_dict['db']['password']
+        self.postgres_db: str = self.config_dict['db']['name']
+        self.database_url: str = self.config_dict['db']['url']
 
-class InfraSettings(BaseSettings):
-    """infrastructure settings(db, etc...)"""
+        # cache
+        self.redis_host: str = self.config_dict['cache']['host']
+        self.redis_port: int = self.config_dict['cache']['port']
+        self.redis_password: str = self.config_dict['cache']['password']
 
-    # DB
-    POSTGRES_USER: str
-    POSTGRES_PW: str
-    POSTGRES_DB: str
-    DATABASE_URL: str
+        # jwt
+        self.access_token_ttl: int = self.config_dict['jwt']['access_token_ttl']
+        self.refresh_token_ttl: int = self.config_dict['jwt']['refresh_token_ttl']
+        self.jwt_key: str = self.config_dict['jwt']['key']
+        self.jwt_algo: str = self.config_dict['jwt']['algo']
 
-    # cache
-    REDIS_HOST: str
-    REDIS_PORT: int
-    REDIS_PASSWORD: str
-
-    # jwt
-    ACCESS_TOKEN_TTL: int
-    REFRESH_TOKEN_TTL: int
-    JWT_KEY: str
-    JWT_ALGO: str
-
-    model_config = SettingsConfigDict(
-        env_file=os.path.join(ENVS_DIR, f".env.{ENV}.infra"),
-        env_file_encoding="utf-8",
-        extra="ignore",  # 目前还没在cls中定义的ENV变量可以忽略报错
-    )
+        # cors
+        self.cors_allow_origins: list[str] = self.config_dict['cors']['allow_origins']
+        self.cors_allow_methods: list[str] = self.config_dict['cors']['allow_methods']
+        self.cors_allow_credentials: bool = self.config_dict['cors']['allow_credentials']
+        self.cors_allow_headers: list[str] = self.config_dict['cors']['allow_headers']
 
 
 # use module level singleton
-app_settings = AppSettings()
-infra_settings = InfraSettings()
+settings = Settings()
