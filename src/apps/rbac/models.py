e7d9f3cb22ede13b07055ce99b5e532d25b8c767
jackcizon <jack20021213cn@gmail.com>
2026-03-13 00:26:40 +0800

    feat: migrate rbac models

---
+++ src/apps/rbac/models.py
@@ -0,0 +1,47 @@
+from sqlalchemy import String, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.core.database import Base
+from src.utils.models import BaseModel
+
+
+class Role(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(16))
+
+    __tablename__ = "Rbac_Role"
+
+
+class Permission(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    code: Mapped[str] = mapped_column(String(256))
+    url: Mapped[str] = mapped_column(String(256), doc="this one will be deprecated")
+
+    __tablename__ = "Rbac_Permission"
+
+
+class User(BaseModel):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(16))
+
+    __tablename__ = "Rbac_User"
+
+
+class User2Role(Base):
+    """no fk constraint, but do more works."""
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(index=True)
+    role_id: Mapped[int] = mapped_column(index=True)
+
+    __tablename__ = "Rbac_User2Role"
+    __table_args__ = (UniqueConstraint("user_id", "role_id", name="unique_user_role"),)
+
+
+class Role2Permission(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    role_id: Mapped[int] = mapped_column(index=True)
+    permission_id: Mapped[int] = mapped_column(index=True)
+
+    __tablename__ = "Rbac_Role2Permission"
+    __table_args__ = (UniqueConstraint("role_id", "permission_id", name="unique_role_permission"),)
