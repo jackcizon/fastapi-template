aaa15286d37b2eeaaf0dc05222dcfd31f06b3e63
jackcizon <jack20021213cn@gmail.com>
2026-03-17 00:54:15 +0800

    feat: add routers.

--- src/core/routes.py
+++ src/core/routes.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
-from src.apps.demo.routes import demos_router
+from src.apps.auth.routes import auth_router
+from src.apps.rbac.routes import rbac_router
 
 
 def include_routers(app_: FastAPI) -> None:
     """similar to Django urls.py urlpatterns, but better"""
-    app_.include_router(router=demos_router, prefix="/demos", tags=["demos"])
+    app_.include_router(router=auth_router, prefix="/auth", tags=["auth"])
+    app_.include_router(router=rbac_router, prefix="/rbac", tags=["rbac"])
