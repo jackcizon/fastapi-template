937cbbcef1904bb14e1db6be29e3f2156bb83251
jackcizon <jack20021213cn@gmail.com>
2026-03-16 15:40:44 +0800

    docs: add comment for root routes.

--- src/core/routes.py
+++ src/core/routes.py
@@ -4,4 +4,5 @@ from src.apps.demo.routes import demos_router
 
 
 def include_routers(app_: FastAPI) -> None:
+    """similar to Django urls.py urlpatterns, but better"""
     app_.include_router(router=demos_router, prefix="/demos", tags=["demos"])
