03a6610202573806abddb5d1053f8c3fdcf49167
jackcizon <jack20021213cn@gmail.com>
2026-03-17 00:34:08 +0800

    feat: jwt auth dep.

--- src/utils/dependencies/auth.py
+++ src/utils/dependencies/auth.py
@@ -4,19 +4,19 @@ from jwt import ExpiredSignatureError, InvalidTokenError
 from src.utils.datastructures.json_web_token import JSONWebToken
 
 
-def get_current_user(request: Request) -> str:
+def jwt_required_dep(request: Request) -> str:
     """
     :param request: FastAPI Request
-    :return: str: user.open_id
+    :return: str: user_id
     """
     auth = request.headers.get("Authorization")
     if not auth or not auth.startswith("Bearer "):
         raise HTTPException(401, "Not authenticated")
 
-    token = auth.split(" ", 1)[1]
+    token = auth.split(" ")[1]
 
     try:
-        payload = JSONWebToken().decode_token(token=token)
+        payload = JSONWebToken.decode_token(token)
     except ExpiredSignatureError:
         raise HTTPException(401, "Token expired")
     except InvalidTokenError:
@@ -25,8 +25,9 @@ def get_current_user(request: Request) -> str:
     # 2. payload type check
     if payload.get("type") != "access":
         raise HTTPException(status_code=401, detail="token type must be access")
-    user_open_id = payload.get("id")
-    if not user_open_id:
+    user_id = payload.get("user_id")
+
+    if not user_id:
         raise HTTPException(401, "Invalid token payload")
 
-    return user_open_id
+    return user_id
