82adfd4614ba2bc6a9358013eefacc4cc5e13dd7
jackcizon <jack20021213cn@gmail.com>
2026-03-16 22:48:43 +0800

    feat: add utils for password.

---
+++ src/utils/securities/password.py
@@ -0,0 +1,22 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class Password:
+    @staticmethod
+    def hash(password: str) -> str:
+        return pwd_context.hash(password)
+
+    @staticmethod
+    def verify(plain_password: str, hashed_password: str) -> bool:
+        return pwd_context.verify(plain_password, hashed_password)
+
+
+if __name__ == '__main__':
+    pwd = '123456'
+    # wrong
+    assert (Password.hash(pwd) == Password.hash(pwd)) is False
+    hashed_pwd = Password.hash(pwd)
+    # right
+    assert (Password.verify(pwd, hashed_pwd)) is True
