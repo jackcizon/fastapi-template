67cdfe2fc1b4d9cee7aff2c29747cc525a4d1f14
jackcizon <jack20021213cn@gmail.com>
2026-03-17 01:05:44 +0800

    lint & style

--- alembic/versions/3c758a86a5fa_.py
+++ alembic/versions/3c758a86a5fa_.py
@@ -5,6 +5,7 @@ Revises: 7b4190e57466
 Create Date: 2026-03-16 20:11:21.975286
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,8 +13,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '3c758a86a5fa'
-down_revision: Union[str, Sequence[str], None] = '7b4190e57466'
+revision: str = "3c758a86a5fa"
+down_revision: Union[str, Sequence[str], None] = "7b4190e57466"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
--- alembic/versions/7f5f45764483_.py
+++ alembic/versions/7f5f45764483_.py
@@ -5,6 +5,7 @@ Revises: 3c758a86a5fa
 Create Date: 2026-03-16 22:01:05.371264
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,8 +13,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '7f5f45764483'
-down_revision: Union[str, Sequence[str], None] = '3c758a86a5fa'
+revision: str = "7f5f45764483"
+down_revision: Union[str, Sequence[str], None] = "3c758a86a5fa"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
