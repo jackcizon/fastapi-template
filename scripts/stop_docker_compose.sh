08848dee4c5b718d4a1cba752005b57927e96f9c
jackcizon <jack20021213cn@gmail.com>
2026-03-18 13:12:18 +0800

    fix: typos corrected.

--- scripts/stop_docker_compose.sh
+++ scripts/stop_docker_compose.sh
@@ -1,4 +1,4 @@
-# please modify vars in envs before running this scripts.
+# please modify vars in envs before running this script.
 docker compose \
   --env-file ../envs/.env.docker.dev \
   -f ../docker/compose.yaml \
