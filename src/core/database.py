ef6538755fa1cdbeda9fae700573df6263daf84b
jackcizon <jack20021213cn@gmail.com>
2026-03-19 18:50:53 +0800

    feat: add context for get_db()

--- src/core/database.py
+++ src/core/database.py
@@ -1,5 +1,6 @@
 """config for database"""
 
+import contextlib
 from typing import Generator
 
 from sqlalchemy.engine.create import create_engine
@@ -12,13 +13,23 @@ engine = create_engine(settings.database_url, pool_pre_ping=True)
 SessionLocal = sessionmaker(engine)
 
 
+@contextlib.contextmanager
 def get_db() -> Generator:
-    """db deps"""
+    """
+    A Route/Script(with Depends()/ContextManager() -> context) -> Service -> Repo -> DB
+
+    call it in routes via `db = Depends(get_db)`.
+    or
+    call it in scripts via `with get_db() as db:`.
+
+    `with SessionLocal() as db:` is not recommend usually, because you need to manually manage `try, except, finally`.
+    """
     db = SessionLocal()
     try:
         yield db
-        db.commit()
-    except Exception:
+        db.commit()  # comment this line if you want to manually commit in `with` code block
+    except Exception as e:
+        print(e)
         db.rollback()
         raise  # any exceptions
     finally:
