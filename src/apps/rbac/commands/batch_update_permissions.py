d25530111f05da438b36ccb3d02bbb1fe77fbdb9
jackcizon <jack20021213cn@gmail.com>
2026-03-19 18:54:09 +0800

    feat: add cmd in batch_update_permissions.py

--- src/apps/rbac/commands/batch_update_permissions.py
+++ src/apps/rbac/commands/batch_update_permissions.py
@@ -1,42 +1,122 @@
 from typing import Any
 
+from click import Command, Context
 from fastapi.routing import APIRoute
+from sqlalchemy import text
 from starlette.routing import Route
 
+from src.core.database import SessionLocal
 from src.main import app
 from src.utils.constants import ROLE_CHILD_MAP, PASSED_APP_PERMISSIONS_CHECK
-from src.utils.datastructures.permission_info import PermissionInfo
 from src.utils.helpers import get_superior_roles
 
-# TODO: wrap into a Command
-def batch_update_permissions() -> None:
-    permissions: list[PermissionInfo] = []
-    app_routes: list[Route | APIRoute | Any] = app.instance.routes
-    for app_route in app_routes:
-        # pass some fastapi built-in routes like 'openapi, swagger, redoc'
-        if not isinstance(app_route, APIRoute):
-            continue
 
-        # auth app must pass
-        app_name = app_route.path.split("/")[1]
-        if app_name in PASSED_APP_PERMISSIONS_CHECK:
-            continue
+class BatchUpdatePermissionsCommand(Command):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.help = "batch update rbac permissions"
 
-        code = app_route.name
-        metadata: dict[str, str] | None = app_route.openapi_extra
+    def invoke(self, ctx: Context) -> Any:
+        # permissions: list[PermissionInfo] = []
+        valid_codes: list[str] = []
+        roles_dict: dict[str, set[str]] = {}
 
-        if metadata is None:
-            role = "user"
-        else:
-            role = metadata.get("role", "user")
+        app_routes: list[Route | APIRoute | Any] = app.instance.routes
+        for app_route in app_routes:
+            # pass some fastapi built-in routes like 'openapi, swagger, redoc'
+            if not isinstance(app_route, APIRoute):
+                continue
 
-        role_parents_set = get_superior_roles(role, ROLE_CHILD_MAP)
+            # auth app must pass
+            app_name = app_route.path.split("/")[
+                1
+            ]  # app name must in urlpath, e.g.: app:demo, url:/demo
+            if app_name in PASSED_APP_PERMISSIONS_CHECK:
+                continue
 
-        permissions.append(PermissionInfo(code=code, role_parents_set=role_parents_set))
+            code = app_route.name
+            metadata: dict[Any, Any] | None = app_route.openapi_extra
 
-        for p in permissions:
-            print(f"{p.role_parents_set} => {p.code}")
+            if metadata is None:
+                role = "user"
+            else:
+                role = metadata.get("role", "user")
 
+            role_parents_set = get_superior_roles(role, ROLE_CHILD_MAP)
 
-if __name__ == "__main__":
-    batch_update_permissions()
+            # permissions.append(PermissionInfo(code=code, role_parents_set=role_parents_set))
+            valid_codes.append(code)
+            roles_dict[role] = role_parents_set
+
+        with SessionLocal() as db:
+            try:
+                if valid_codes is None:
+                    db.execute(text('DELETE FROM "Rbac_Permission" where id > 0;'))
+                    db.execute(text('DELETE FROM "Rbac_Role2Permission" where id > 0;'))
+                    db.commit()
+                    return
+
+                # upsert Permission
+                code_values = ",".join(
+                    f"('{code}')" for code in valid_codes
+                )  # ('auth:login'),('auth:register'), ...
+                upsert = text(f"""
+                INSERT INTO "Rbac_Permission"(code)
+                VALUES {code_values}
+                ON CONFLICT(code) DO NOTHING;
+                """)
+                db.execute(upsert)
+
+                # del dirty data
+                codes = ",".join(
+                    f"'{code}'" for code in valid_codes
+                )  # 'auth:login','auth:register', ...
+                del_dirties = text(f"""
+                DELETE FROM "Rbac_Permission"
+                WHERE code NOT IN ({codes});
+                """)
+                db.execute(del_dirties)
+
+                # Role2Permission
+                role_perm_pairs = []
+                for code in valid_codes:
+                    for role, parents in roles_dict.items():
+                        for parent_role in parents:
+                            # [('cto', 'auth:login'), ('user', 'auth:login'), ('staff', 'auth:login'), ...]
+                            role_perm_pairs.append((parent_role, code))
+
+                if not role_perm_pairs:
+                    db.commit()
+                    return
+
+                # update Role2Permission
+                values_clause = ",".join(
+                    f"('{role_name}', '{perm_code}')" for role_name, perm_code in role_perm_pairs
+                )  # ('chairman', 'auth:login'),('ceo', 'auth:login'),('cto', 'auth:login'), ...
+                upsert = text(f"""
+                INSERT INTO "Rbac_Role2Permission"(role_id, permission_id)
+                SELECT role.id, perm.id
+                FROM (VALUES {values_clause}) AS tmp(role_name, perm_code)
+                JOIN "Rbac_Role" role ON role.name = tmp.role_name
+                JOIN "Rbac_Permission" perm ON perm.code = tmp.perm_code
+                ON CONFLICT(role_id, permission_id) DO NOTHING;
+                """)
+                db.execute(upsert)
+
+                # del dirty data
+                del_dirties = text(f"""
+                DELETE FROM "Rbac_Role2Permission"
+                WHERE (role_id, permission_id) NOT IN (
+                SELECT role.id, perm.id
+                FROM (VALUES {values_clause}) AS tmp(role_name, perm_code)
+                JOIN "Rbac_Role" role ON role.name = tmp.role_name
+                JOIN "Rbac_Permission" perm ON perm.code = tmp.perm_code);
+                """)
+                db.execute(del_dirties)
+
+                db.commit()
+            except Exception as e:
+                db.rollback()
+                raise e
+
+        return super().invoke(ctx)
