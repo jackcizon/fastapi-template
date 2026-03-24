63c251fdf7ff9b813a5598770bf7bfb624ee3ad6
jackcizon <jack20021213cn@gmail.com>
2026-03-25 00:09:05 +0800

    chore: first migration

--- src/apps/migrations/versions/70e51b596a6d_empty_message.py
+++ src/apps/migrations/versions/70e51b596a6d_empty_message.py
@@ -1,10 +1,11 @@
 """empty message
 
 Revision ID: 70e51b596a6d
-Revises: 
+Revises:
 Create Date: 2026-03-25 00:04:43.215697
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,7 +13,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '70e51b596a6d'
+revision: str = "70e51b596a6d"
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
