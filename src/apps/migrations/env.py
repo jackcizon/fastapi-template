da7f9f63188a9720a6f54d6cdd70a97f8251a56d
jackcizon <jack20021213cn@gmail.com>
2026-03-25 00:07:45 +0800

    chore: del old migrations

--- src/apps/migrations/env.py
+++ src/apps/migrations/env.py
@@ -18,12 +18,10 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-# TODO: import Base and models
 from src.core.database import Base
-from src.apps.models import *
+from src.apps.models import *  # must include
 
 target_metadata = Base.metadata
-
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -69,7 +67,9 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
 
         with context.begin_transaction():
             context.run_migrations()
