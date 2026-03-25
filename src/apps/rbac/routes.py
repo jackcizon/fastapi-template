0c3402ddc25616288d8a06f0e50be137ea8d9355
jackcizon <jack20021213cn@gmail.com>
2026-03-26 04:34:51 +0800

    fix: role_permission_check_dep bug fixed.

--- src/apps/rbac/routes.py
+++ src/apps/rbac/routes.py
@@ -3,12 +3,12 @@ from fastapi.params import Depends
 from starlette.responses import JSONResponse
 
 from src.apps.rbac.models import User
-from src.core.securities.rbac import RolePermissionCheck
+from src.core.securities.rbac import role_permission_check_dep
 
 rbac_router = APIRouter()
 
 
 @rbac_router.get("/index/", name="rbac:index", openapi_extra={"role": "staff"})
-def rbac_index(user_role: tuple[User, str] = Depends(RolePermissionCheck())) -> JSONResponse:
+def rbac_index(user_role: tuple[User, str] = Depends(role_permission_check_dep)) -> JSONResponse:
     user, role = user_role
     return JSONResponse(content={"user": user.id, "role": role})
--- src/core/securities/rbac.py
+++ src/core/securities/rbac.py
@@ -11,46 +11,24 @@ from src.core.securities.jwt import jwt_required_dep
 from src.core.constants import DEFAULT_ROLE
 
 
-class RolePermissionCheck:
-    """
-    :return: tuple(user_instance, min_req_role_str)
-    """
-
-    def __init__(self) -> None:
-        self._user: User | None = None
-        self._permission_code: str | None = None
-        self._role: str | None = None
-        self._passed: bool = False
-
-    def assign_args(self, request: Request, user: User) -> None:
-        self._user = user
-        route: APIRoute = request.scope.get("route")
-        self._permission_code = route.name
-        metadata: dict[Any, Any] | None = route.openapi_extra
-        if metadata is None:
-            self._role = DEFAULT_ROLE
-        else:
-            self._role = metadata.get("role")
-
-    def _has_permission(self, db: Session, user: User) -> bool:
-        # TODO: store in cache, query from cache, not db.
-        user_permissions = RbacRepo(db).get_user_permissions(user)
-        codes = [user_permission[0] for user_permission in user_permissions]
-
-        if self._permission_code in codes:
-            self._passed = True
-        return self._passed
-
-    def _result(self) -> tuple[User, str]:
-        return self._user, self._role
-
-    def __call__(
-        self,
+def role_permission_check_dep(
         request: Request,
         db: Session = Depends(get_db),
         user: User = Depends(jwt_required_dep),
-    ) -> tuple[User, str]:
-        self.assign_args(request, user)
-        if not self._has_permission(db, user):
-            raise HTTPException(status_code=403, detail="403 Forbidden")
-        return self._result()
+) -> tuple[User, str]:
+    """
+    :return: tuple(user_instance, min_req_role_str)
+    """
+    route: APIRoute = request.scope.get("route")
+    permission_code = route.name
+    metadata: dict[Any, Any] | None = route.openapi_extra
+    if metadata is None:
+        role = DEFAULT_ROLE
+    else:
+        role = metadata.get("role")
+
+    user_permissions = RbacRepo(db).get_user_permissions(user)
+    codes = [user_perm for user_perm in user_permissions]
+    if permission_code not in codes:
+        raise HTTPException(status_code=403, detail="403 Forbidden")
+    return user, role
