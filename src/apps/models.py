6b819edc809ce5181907fc642b6031bb0f921315
jackcizon <jack20021213cn@gmail.com>
2026-03-25 14:01:11 +0800

    chore: code clean up

--- src/apps/models.py
+++ src/apps/models.py
@@ -1,5 +1,4 @@
 """for migrations"""
 
 # TODO: import all models from this file.
-# from src.apps.auth.models import *
 from src.apps.rbac.models import *
--- src/utils/models.py
+++ src/utils/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, Boolean, DateTime, func
+from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.core.database import Base
