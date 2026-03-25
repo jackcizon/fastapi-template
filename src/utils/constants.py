568a1a22f4290ecc7162cdb91427b5f6979affef
jackcizon <jack20021213cn@gmail.com>
2026-03-25 14:02:37 +0800

    feat: update constants.py role related const

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,5 +1,7 @@
 # this map needs to recursion to get the role hierarchy
-ROLE_CHILD_MAP = {
+from typing import Final, Any
+
+ROLE_CHILD_MAP: Final[dict[str, list[Any]]] = {
     # role: [child1, child2] or []
     "user": [],
     "staff": ["user"],
@@ -9,9 +11,11 @@ ROLE_CHILD_MAP = {
     "chairman": ["ceo"],
 }
 
-PASSED_APP_PERMISSIONS_CHECK: list[str] = [
+DEFAULT_ROLES: Final[list[str]] = list(ROLE_CHILD_MAP.keys())
+
+DEFAULT_ROLE: Final[str] = DEFAULT_ROLES[0]
+
+PASSED_APP_PERMISSIONS_CHECK: Final[list[str]] = [
     "alembic",
     "migrations",
 ]  # these apps will ignore permissions update.
-
-DEFAULT_ROLE: str = "user"
