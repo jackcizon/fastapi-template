e88342b0ed66040ad681dd49f319547b3f44852b
jackcizon <jack20021213cn@gmail.com>
2026-03-27 11:55:59 +0800

    docs: add docs for src:main:app

--- src/main.py
+++ src/main.py
@@ -3,4 +3,6 @@
 from src.core.config import settings
 from src.core.application.lazy_load_app import LazyLoadApp
 
+# app factory
+# use `.instance` to get `app:FastAPI instance`
 app = LazyLoadApp(debug=settings.debug)
