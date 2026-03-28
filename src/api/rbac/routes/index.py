d273670c7ecdbb31a58be73e1cd2cec6d5c290a0
jackcizon <jack20021213cn@gmail.com>
2026-03-28 21:19:45 +0800

    refactor: replace `pure func dep` to `cls dep, func wrapper` in cls:RolePermissionCheck

--- src/api/rbac/routes/index.py
+++ src/api/rbac/routes/index.py
@@ -3,12 +3,12 @@ from fastapi.params import Depends
 from starlette.responses import JSONResponse
 
 from src.api.rbac.models import User
-from src.core.securities.rbac import role_permission_check_dep
+from src.core.securities.rbac import role_prem_check_wrapper_dep
 
 index_router = APIRouter()
 
 
 @index_router.get("/index/", name="rbac:index", openapi_extra={"role": "staff"})
-def rbac_index(user_role: tuple[User, str] = Depends(role_permission_check_dep)) -> JSONResponse:
+def rbac_index(user_role: tuple[User, str] = Depends(role_prem_check_wrapper_dep)) -> JSONResponse:
     user, role = user_role
     return JSONResponse(content={"user": user.id, "role": role})
