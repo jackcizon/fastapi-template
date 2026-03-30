95cd15533bb13f543410ecaa483f8002e74c57e9
jackcizon <jack20021213cn@gmail.com>
2026-03-30 18:46:48 +0800

    refactor: base repo.

--- src/core/db/repo/base.py
+++ src/core/db/repo/base.py
@@ -1,34 +1,44 @@
-from typing import Any
+from typing import Any, Generic, Sequence
 
 from sqlalchemy import delete, select
 from sqlalchemy.orm import Session
 
+from src.core.types import Model
 
-class BaseRepo:
-    def __init__(self, db: Session, /, *, model: Any = None):
+
+class BaseRepo(Generic[Model]):
+    def __init__(self, db: Session, /, *, model: type[Model] = None):
         self.db = db
         self.model = model
 
+    def column(self, field: str) -> Any:
+        return getattr(self.model, field)
 
-class QueryRepo(BaseRepo):
+
+class QueryRepo(BaseRepo[Model]):
     """select"""
 
-    def get_by_id(self, id_: int) -> Any:
-        stat = (
-            select(self.model).where(self.model.id == id_)  # type: ignore
-        )
+    def get_one_by_field_eq(self, field: str, val: str) -> Model:
+        stat = select(self.model).where(self.column(field) == val)  # type: ignore
         return self.db.execute(stat).scalars().first()
 
+    def get_all_by_field_eq(self, field: str, val: str) -> Sequence[Model]:
+        stat = select(self.model).where(self.column(field) == val)  # type: ignore
+        return self.db.execute(stat).scalars().all()
 
-class ModifyRepo(BaseRepo):
+    def get_all_by_field_in(self, field: str, vals: list[str]) -> Sequence[Model]:
+        col = getattr(self.model, field)
+        stat = select(self.model).where(col.in_(vals))
+        return self.db.execute(stat).scalars().all()
+
+
+class ModifyRepo(BaseRepo[Model]):
     """update, delete, insert"""
 
     def del_all(self) -> None:
-        stat = delete(self.model).where(self.model.id > 0)
+        stat = delete(self.model).where(self.column("id") > 0)
         self.db.execute(stat)
 
 
-class CrudRepo(QueryRepo, ModifyRepo):
+class CrudRepo(QueryRepo[Model], ModifyRepo[Model]):
     """crud"""
-
-    pass
