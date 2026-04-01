4554b31e16a1ab91e4efd4fdbfbfbb5daf21cec9
jackcizon <jack20021213cn@gmail.com>
2026-04-01 19:19:56 +0800

    chore: del old tests

--- tests/api/rbac/test_repos/test_rbac_repo.py
+++ tests/api/rbac/test_repos/test_rbac_repo.py
@@ -7,63 +7,64 @@ from src.api.rbac.repos.role2permission_repo import Role2PermissionRepo
 
 
 class TestRbacRepo:
-    def test_init(self, test_db):
-        repo = RbacRepo(test_db)
-        assert repo.user
-        assert repo.permission
-        assert repo.user_role
-        assert repo.role_permission
-
-    @staticmethod
-    def _insert_user(test_db):
-        stat = Insert(User)
-        params = {"id": 1, "name": "aafddsfdsa", "password": "isufuhsdiufvds", "email": "cjsjdf@de.com"}
-        test_db.execute(stat, params)
-        test_db.commit()
-
-    @staticmethod
-    def _insert_role_perm(test_db):
-        stat = insert(Role)
-        params = [{"id": 1, "name": "chairman"}]
-        test_db.execute(stat, params)
-        test_db.commit()
-
-        stat = Insert(Permission)
-        params = {"code": "auth:login"}
-        test_db.execute(stat, params)
-        test_db.commit()
-
-    @staticmethod
-    def _insert_role2perm(test_db):
-        role_perm_pairs: list[tuple[str, str]] = [("chairman", "auth:login")]
-        repo = Role2PermissionRepo(test_db)
-
-        repo.upsert_by_role_perm_pairs(role_perm_pairs)
-        test_db.commit()
-
-    @staticmethod
-    def _insert_user2role(test_db, user_id, role_id):
-        stat = insert(User2Role)
-        params = [{"role_id": role_id, "user_id": user_id}]
-        test_db.execute(stat, params)
-        test_db.commit()
-
-    @staticmethod
-    def _get_user(test_db, id_):
-        stat = select(User)
-        params = {"id": id_}
-        user = test_db.execute(stat, params).scalar_one()
-        return user
-
-    def test_get_user_permissions(self, test_db):
-        self._insert_user(test_db)
-        self._insert_role_perm(test_db)
-        self._insert_user2role(test_db, 1, 1)
-        self._insert_role2perm(test_db)
-
-        # user_perms = [('rbac:index',), ('auth:me',), ('auth:register',), ('auth:login',)]
-        repo = RbacRepo(test_db)
-
-        user = self._get_user(test_db, 1)
-        res = repo.get_user_permissions(user)
-        print(res)
+    pass
+    # def test_init(self, test_db):
+    #     repo = RbacRepo(test_db)
+    #     assert repo.user
+    #     assert repo.permission
+    #     assert repo.user_role
+    #     assert repo.role_permission
+    #
+    # @staticmethod
+    # def _insert_user(test_db):
+    #     stat = Insert(User)
+    #     params = {"id": 1, "name": "aafddsfdsa", "password": "isufuhsdiufvds", "email": "cjsjdf@de.com"}
+    #     test_db.execute(stat, params)
+    #     test_db.commit()
+    #
+    # @staticmethod
+    # def _insert_role_perm(test_db):
+    #     stat = insert(Role)
+    #     params = [{"id": 1, "name": "chairman"}]
+    #     test_db.execute(stat, params)
+    #     test_db.commit()
+    #
+    #     stat = Insert(Permission)
+    #     params = {"code": "auth:login"}
+    #     test_db.execute(stat, params)
+    #     test_db.commit()
+    #
+    # # @staticmethod
+    # # def _insert_role2perm(test_db):
+    # #     role_perm_pairs: list[tuple[str, str]] = [("chairman", "auth:login")]
+    # #     repo = Role2PermissionRepo(test_db)
+    # #
+    # #     repo.upsert_by_role_perm_pairs(role_perm_pairs)
+    # #     test_db.commit()
+    #
+    # @staticmethod
+    # def _insert_user2role(test_db, user_id, role_id):
+    #     stat = insert(User2Role)
+    #     params = [{"role_id": role_id, "user_id": user_id}]
+    #     test_db.execute(stat, params)
+    #     test_db.commit()
+    #
+    # @staticmethod
+    # def _get_user(test_db, id_):
+    #     stat = select(User)
+    #     params = {"id": id_}
+    #     user = test_db.execute(stat, params).scalar_one()
+    #     return user
+    #
+    # # def test_get_user_permissions(self, test_db):
+    # #     self._insert_user(test_db)
+    # #     self._insert_role_perm(test_db)
+    # #     self._insert_user2role(test_db, 1, 1)
+    # #     self._insert_role2perm(test_db)
+    #
+    #     # user_perms = [('rbac:index',), ('auth:me',), ('auth:register',), ('auth:login',)]
+    #     repo = RbacRepo(test_db)
+    #
+    #     user = self._get_user(test_db, 1)
+    #     res = repo.get_user_permissions(user)
+    #     print(res)
--- tests/api/rbac/test_repos/test_role2permission_repo.py
+++ tests/api/rbac/test_repos/test_role2permission_repo.py
@@ -14,67 +14,68 @@ class TestRole2PermissionRepo:
     3. Hard-coded IDs are a contract in my test code: I define: User_id is always 1, Role_id is always 1.
     """
 
-    @staticmethod
-    def _insert_roles_and_perms(test_db):
-        stat = Insert(Role)
-        params = [{"name": "chairman"}, {"name": "ceo"}, {"name": "cto"}]
-        stat = stat.on_conflict_do_nothing(index_elements=["name"])
-        test_db.execute(stat, params)
-        test_db.commit()
-
-        stat = Insert(Permission)
-        params = {"code": "auth:login"}
-        test_db.execute(stat, params)
-        test_db.commit()
-
-    @staticmethod
-    def _upsert(test_db):
-        role_perm_pairs: list[tuple[str, str]] = [
-            ("chairman", "auth:login"),
-            ("ceo", "auth:login"),
-            ("cto", "auth:login"),
-        ]
-        repo = Role2PermissionRepo(test_db)
-
-        repo.upsert_by_role_perm_pairs(role_perm_pairs)
-        test_db.commit()
-
-        modified_role_perm_pairs_test_on_conflict = [("chairman", "auth:login")]
-        repo.upsert_by_role_perm_pairs(modified_role_perm_pairs_test_on_conflict)
-        test_db.commit()
-
-    def test_upsert_role_perm_pairs(self, test_db):
-        self._insert_roles_and_perms(test_db)
-        self._upsert(test_db)
-
-        stat = Select(Role2Permission.role_id, Role2Permission.permission_id)
-        res = test_db.execute(stat).mappings().all()
-        assert res is not None
-
-    def test_del_dirty_data(self, test_db):
-        self._insert_roles_and_perms(test_db)
-        self._upsert(test_db)
-
-        # dirty data
-        role_perm_pairs: list[tuple[str, str]] = [
-            ("chairman", "auth:login111111111"),
-            ("ceo", "auth:login"),
-            ("cto", "auth:login"),
-        ]
-        repo = Role2PermissionRepo(test_db)
-
-        repo.upsert_by_role_perm_pairs(role_perm_pairs)
-        test_db.commit()
-
-        role_perm_pairs: list[tuple[str, str]] = [
-            ("chairman", "auth:login"),
-            ("ceo", "auth:login"),
-            ("cto", "auth:login"),
-        ]
-        repo = Role2PermissionRepo(test_db)
-
-        repo.upsert_by_role_perm_pairs(role_perm_pairs)
-        test_db.commit()
-
-        repo.del_dirty_data(role_perm_pairs)
-        test_db.commit()
+    #
+    # @staticmethod
+    # def _insert_roles_and_perms(test_db):
+    #     stat = Insert(Role)
+    #     params = [{"name": "chairman"}, {"name": "ceo"}, {"name": "cto"}]
+    #     stat = stat.on_conflict_do_nothing(index_elements=["name"])
+    #     test_db.execute(stat, params)
+    #     test_db.commit()
+    #
+    #     stat = Insert(Permission)
+    #     params = {"code": "auth:login"}
+    #     test_db.execute(stat, params)
+    #     test_db.commit()
+    #
+    # @staticmethod
+    # def _upsert(test_db):
+    #     role_perm_pairs: list[tuple[str, str]] = [
+    #         ("chairman", "auth:login"),
+    #         ("ceo", "auth:login"),
+    #         ("cto", "auth:login"),
+    #     ]
+    #     repo = Role2PermissionRepo(test_db)
+    #
+    #     repo.upsert_by_role_perm_pairs(role_perm_pairs)
+    #     test_db.commit()
+    #
+    #     modified_role_perm_pairs_test_on_conflict = [("chairman", "auth:login")]
+    #     repo.upsert_by_role_perm_pairs(modified_role_perm_pairs_test_on_conflict)
+    #     test_db.commit()
+    #
+    # def test_upsert_role_perm_pairs(self, test_db):
+    #     self._insert_roles_and_perms(test_db)
+    #     self._upsert(test_db)
+    #
+    #     stat = Select(Role2Permission.role_id, Role2Permission.permission_id)
+    #     res = test_db.execute(stat).mappings().all()
+    #     assert res is not None
+    #
+    # def test_del_dirty_data(self, test_db):
+    #     self._insert_roles_and_perms(test_db)
+    #     self._upsert(test_db)
+    #
+    #     # dirty data
+    #     role_perm_pairs: list[tuple[str, str]] = [
+    #         ("chairman", "auth:login111111111"),
+    #         ("ceo", "auth:login"),
+    #         ("cto", "auth:login"),
+    #     ]
+    #     repo = Role2PermissionRepo(test_db)
+    #
+    #     repo.upsert_by_role_perm_pairs(role_perm_pairs)
+    #     test_db.commit()
+    #
+    #     role_perm_pairs: list[tuple[str, str]] = [
+    #         ("chairman", "auth:login"),
+    #         ("ceo", "auth:login"),
+    #         ("cto", "auth:login"),
+    #     ]
+    #     repo = Role2PermissionRepo(test_db)
+    #
+    #     repo.upsert_by_role_perm_pairs(role_perm_pairs)
+    #     test_db.commit()
+    #
+    #     repo.del_dirty_data(role_perm_pairs)
+    #     test_db.commit()
