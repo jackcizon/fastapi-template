97130a5fd4fafdbf9d922a7e34ee4a7408d9b4df
jackcizon <jack20021213cn@gmail.com>
2026-04-03 00:54:35 +0800

    fix: ci TEST_DATABASE_URL

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,6 +15,10 @@ from src.main import app
 test_settings = Settings("test")
 
 TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL", test_settings.database_url)  # ci docker test db
+if TEST_DATABASE_URL.startswith("postgresql://"):
+    TEST_DATABASE_URL = TEST_DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
+elif TEST_DATABASE_URL.startswith("postgresql+psycopg2://"):
+    TEST_DATABASE_URL = TEST_DATABASE_URL.replace("postgresql+psycopg2://", "postgresql+asyncpg://", 1)
 
 
 @pytest.fixture(scope="session", autouse=True)
