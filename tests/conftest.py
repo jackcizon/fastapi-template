d369804bf7c07e69e5f40b327af72a37b79b3fd3
jackcizon <jack20021213cn@gmail.com>
2026-04-03 00:57:43 +0800

    chore: add comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,7 @@ from src.main import app
 
 test_settings = Settings("test")
 
+# use asyncpg
 TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL", test_settings.database_url)  # ci docker test db
 if TEST_DATABASE_URL.startswith("postgresql://"):
     TEST_DATABASE_URL = TEST_DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
