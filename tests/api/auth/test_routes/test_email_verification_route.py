64ad21feab9b122acf1f86a1c76c14e36a67232a
jackcizon <jack20021213cn@gmail.com>
2026-04-18 23:43:00 +0800

    test: email verify

--- tests/api/auth/test_routes/test_email_verification_route.py
+++ tests/api/auth/test_routes/test_email_verification_route.py
@@ -1,41 +1,45 @@
 from typing import Final
 
 from fastapi import status
+from sqlalchemy import insert
 
 from src.api.auth.schemas.email_schema import EmailVerificationRequestSchema
+from src.api.rbac.models import User
 
 
 class TestEmailVerification:
     url: Final[str] = "/auth/email_verification/"
 
-    # @staticmethod
-    # async def _insert_one_user(test_db):
-    #     stat = insert(User).values(name="test_user", email="apitest@example.com", password="123456789")
-    #     await test_db.execute(stat)
-    #     await test_db.flush()
+    @staticmethod
+    async def _insert_one_user(test_db):
+        stat = insert(User).values(name="test_user", email="apitest@example.com", password="123456789")
+        await test_db.execute(stat)
+        await test_db.flush()
 
     async def test_email_verification_success(self, test_client, test_db):
-        # await self._insert_one_user(test_db)
+        await self._insert_one_user(test_db)
 
-        input_data = EmailVerificationRequestSchema(email="apitest@example.com").model_dump()
+        input_data = EmailVerificationRequestSchema(email="apitest@example.com", registered=True).model_dump()
         response = await test_client.post(self.url, json=input_data)
 
         assert response.status_code == status.HTTP_200_OK
         resp = response.json()
         assert len(resp["verification_code"]) == 9
 
-    # async def test_email_verification_wrong_email(self, test_client, test_db):
-    #     data = {"email": "notexists@example.com"}
-    #     response = await test_client.post(self.url, json=data)
-    #
-    #     assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    async def test_email_verification_wrong_email(self, test_client, test_db):
+        await self._insert_one_user(test_db)
+
+        input_data = EmailVerificationRequestSchema(email="nonexists@example.com", registered=True).model_dump()
+        response = await test_client.post(self.url, json=input_data)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
     async def test_email_verification_rate_limit(self, test_client, test_db):
-        # await self._insert_one_user(test_db)
+        await self._insert_one_user(test_db)
 
         last_status_code = 0
         for i in range(6):
-            input_data = EmailVerificationRequestSchema(email="apitest@example.com").model_dump()
+            input_data = EmailVerificationRequestSchema(email="apitest@example.com", registered=True).model_dump()
             response = await test_client.post(self.url, json=input_data)
             last_status_code = response.status_code
 
--- tests/api/auth/test_services/test_email_verification_service.py
+++ tests/api/auth/test_services/test_email_verification_service.py
@@ -1,25 +1,30 @@
+import pytest
+from sqlalchemy import Insert
+
 from src.api.auth.schemas.email_schema import EmailVerificationRequest
 from src.api.auth.services.email_verification_service import EmailVerificationService
+from src.api.rbac.models import User
+from src.core.exceptions.auth import AuthError
 
 
 class TestEmailVerificationService:
-    async def test_verify_success(self, test_cache):
-        # params = [{"name": "test", "email": "email@test.com", "password": "123456789"}]
-        # stat = Insert(User)
-        # await test_db.execute(stat, params)
-        # await test_db.flush()
+    async def test_verify_success(self, test_cache, test_db):
+        params = [{"name": "test", "email": "email@test.com", "password": "123456789"}]
+        stat = Insert(User)
+        await test_db.execute(stat, params)
+        await test_db.flush()
 
-        req = EmailVerificationRequest(email="email@test.com")
-        resp = await EmailVerificationService.verify(req, test_cache)
+        req = EmailVerificationRequest(email="email@test.com", registered=True)
+        resp = await EmailVerificationService.verify(req, test_cache, test_db)
         # await EmailVerificationService.notify()  dev does not need to test, a little complex
         assert len(resp["verification_code"]) == 9
 
-    # async def test_verify_failed(self, test_cache):
-    #     # user = User(name="test", email="email@test.com", password="12234567")
-    #     # test_db.add(user)
-    #     # await test_db.flush()
-    #
-    #     req = EmailVerificationRequest(email="11111111@qq.com")
-    #
-    #     with pytest.raises(AuthError):
-    #         await EmailVerificationService.verify(req, test_cache)
+    async def test_verify_failed(self, test_cache, test_db):
+        user = User(name="test", email="email@test.com", password="12234567")
+        test_db.add(user)
+        await test_db.flush()
+
+        req = EmailVerificationRequest(email="11111111@qq.com", registered=True)
+
+        with pytest.raises(AuthError):
+            await EmailVerificationService.verify(req, test_cache, test_db)
