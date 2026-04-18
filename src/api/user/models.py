fd5da1531497935cf39098b05249b9511746869b
jackcizon <jack20021213cn@gmail.com>
2026-04-18 23:38:45 +0800

    feat: userprofile model

--- src/api/user/models.py
+++ src/api/user/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import BigInteger
+from sqlalchemy import BigInteger, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.core.db.models import BaseModel
@@ -6,4 +6,6 @@ from src.core.db.models import BaseModel
 
 class UserProfile(BaseModel):
     id: Mapped[int] = mapped_column(BigInteger, comment="same as Rbac_User.id", primary_key=True, autoincrement=False)
-    # additional fields
+    avatar: Mapped[str] = mapped_column(String(256), default="static/media/avatar/default_avatar.png")
+
+    __tablename__ = "UserProfile"
