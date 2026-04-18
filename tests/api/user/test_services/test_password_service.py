a8a3d5e2f963ef51c47cd965b2c0f0715d88b3bf
jackcizon <jack20021213cn@gmail.com>
2026-04-18 23:47:26 +0800

    test: pwd reset

---
+++ tests/api/user/test_services/test_password_service.py
@@ -0,0 +1,45 @@
+import pytest
+from sqlalchemy import Insert
+
+from src.api.rbac.models import User
+from src.api.rbac.repos.user_repo import UserRepo
+from src.api.user.schemas.password_schema import PasswordResetRequest
+from src.api.user.services.password_service import PasswordService
+from src.core.exceptions.auth import AuthError
+from src.core.rdb.keys import cache_keys
+
+
+class TestPasswordResetService:
+    email = "email@test.com"
+    verification_code = "qazwsxedc"
+    wrong_verification_code = "wrongcode"
+
+    async def _insert(self, test_db):
+        params = [{"name": "test", "email": self.email, "password": "123456789"}]
+        stat = Insert(User)
+        await test_db.execute(stat, params)
+        await test_db.flush()
+
+    async def test_reset_success(self, test_cache, test_db):
+        await self._insert(test_db)
+
+        user = await UserRepo(test_db).get_one_by_field_eq("email", self.email)
+        await test_cache.setex(f"{cache_keys.email_verification_code}:{self.email}", 300, self.verification_code)
+
+        req = PasswordResetRequest(new_password="123456", verification_code=self.verification_code)
+        resp = await PasswordService.reset(req=req, user=user, cache=test_cache, db=test_db)
+        assert resp["status"] is True
+
+    async def test_reset_failed(self, test_cache, test_db):
+        await self._insert(test_db)
+
+        user = await UserRepo(test_db).get_one_by_field_eq("email", self.email)
+        await test_cache.setex(f"{cache_keys.email_verification_code}:{self.email}", 300, self.verification_code)
+
+        req = PasswordResetRequest(new_password="123456", verification_code=self.wrong_verification_code)
+        with pytest.raises(AuthError):
+            await PasswordService.reset(req=req, user=user, cache=test_cache, db=test_db)
+
+
+class TestPasswordForgetService:
+    pass
