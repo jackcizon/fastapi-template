b677cf3ab4027d46ed1ab15f34ed631263a5c85f
jackcizon <jack20021213cn@gmail.com>
2026-05-02 22:14:51 +0800

    style

--- tests/browser/server.py
+++ tests/browser/server.py
@@ -7,5 +7,5 @@ from reactor_server.http_server.eventloop import EventLoop
 
 pwd = dirname(os.path.abspath(__file__))
 
-if __name__ == '__main__':
-    Server(EventLoop, root=pwd, thread_num=2, port=8888, host='0.0.0.0').run()
+if __name__ == "__main__":
+    Server(EventLoop, root=pwd, thread_num=2, port=8888, host="0.0.0.0").run()
